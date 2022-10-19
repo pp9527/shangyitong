3ade6d7012c39b48eef1b4006973388aea00ad6f
pp9527 <2382852105@qq.com>
2022-10-19 15:40:06 +0800

    数据字典完成

--- yygh-parent/service/service_cmn/src/main/java/com/atguigu/yygh/cmn/controller/DictController.java
+++ yygh-parent/service/service_cmn/src/main/java/com/atguigu/yygh/cmn/controller/DictController.java
@@ -6,8 +6,10 @@ import com.atguigu.yygh.model.cmn.Dict;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 /**
@@ -25,11 +27,52 @@ public class DictController {
     @Resource
     private DictService dictService;
 
+    //导入数据字典
+    @PostMapping("importData")
+    @ApiOperation(value = "导入数据字典")
+    public Result importDict(MultipartFile file) {
+        dictService.importDictData(file);
+        return Result.ok();
+    }
+
+    //导出数据字典接口
+    @GetMapping("exportData")
+    @ApiOperation(value = "导出数据字典接口")
+    public void exportDict(HttpServletResponse response) {
+        dictService.exportDictData(response);
+    }
+
+    //根据dictCode获取下级节点
+    @ApiOperation(value = "根据dictCode获取下级节点")
+    @GetMapping("findByDictCode/{dictCode}")
+    public Result findByDictCode(@PathVariable String dictCode) {
+        List<Dict> list = dictService.findByDictCode(dictCode);
+        return Result.ok(list);
+    }
+
     //根据数据id查询子数据列表
     @ApiOperation(value = "根据数据id查询子数据列表")
     @GetMapping("findChildData/{id}")
     public Result findChildData(@PathVariable Long id) {
+        System.out.println(id);
         List<Dict> list = dictService.findChlidData(id);
         return Result.ok(list);
+    }
+
+    //根据dictcode和value查询
+    @ApiOperation(value = "根据dictcode和value查询")
+    @GetMapping("getName/{dictCode}/{value}")
+    public String getName(@PathVariable String dictCode,
+                          @PathVariable String value) {
+        String dictName = dictService.getDictName(dictCode,value);
+        return dictName;
+    }
+
+    //根据value查询
+    @ApiOperation(value = "根据value查询")
+    @GetMapping("getName/{value}")
+    public String getName(@PathVariable String value) {
+        String dictName = dictService.getDictName("",value);
+        return dictName;
     }
 }
